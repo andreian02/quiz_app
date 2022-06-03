d76c6705bb8e1cbaba5f29252b13b73291c4ea2b
Andrew <andrew.lim@tbwa.com>
2022-06-03 09:02:26 +0800

    3rd commit

--- src/routes/quiz.svelte
+++ src/routes/quiz.svelte
@@ -10,7 +10,8 @@
 
 	// we don't need any JS on this page, though we'll load
 	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
+	// export const hydrate = dev;
+	hydrate = true;
 
 	// ...but if the client-side router is already loaded
 	// (i.e. we came here from elsewhere in the app), use it
