c6f638935d5b20d8bd280332f458b37f16e6c5d6
andreian02 <andreian02.lim@gmail.com>
2022-06-22 20:39:57 +0800

    adding new page

---
+++ src/routes/shuo-gua.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>易经說卦傳</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+
+<section>
+	<h1>
+		<div class="flex flex-col text-center space-y-4 pb-3">
+			<div class="welcome">易经說卦傳</div>
+        </div>
+    </h1>
+</section>
